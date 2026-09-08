3ddb09d1e2941b345c5966b38967e189bf68d4ea
khalid-bensaada <kbensaada953@gmail.com>
2026-09-08 17:09:55 +0100

    ajouter daux functions que sa palle getUser et logoutUser

--- js/storage.js
+++ js/storage.js
@@ -2,3 +2,11 @@
 export function saveUser(user){
     localStorage.setItem('user' ,JSON.stringify(user)) ;
 }
+
+export function getUser(){
+    const user = localStorage.getItem('user');
+}
+
+export function logoutUser(){
+    localStorage.removeItem('user');
+}
