1ca3b6022a63a6f817748e8205b971ab5310c016
khalid-bensaada <kbensaada953@gmail.com>
2026-09-10 09:43:59 +0100

    ajouter un route pour login et sign up

--- js/route.js
+++ js/route.js
@@ -1,1 +1,34 @@
-console.log(1);
+import { getUsers } from './storage.js';
+import { authForm, authEvent } from './view/auth.js';
+
+const appContainer = document.getElementById('app');
+
+function getCurrentRoute() {
+    const hash = window.location.hash.replace('#', '');
+    return hash || 'login';
+}
+
+function renderRoute() {
+    const route = getCurrentRoute();
+
+    switch (route) {
+        case 'login':
+            appContainer.innerHTML = authForm('login');
+            authEvent('login');
+            break;
+
+        case 'signup':
+            appContainer.innerHTML = authForm('signup');
+            authEvent('signup');
+            break;
+
+        default:
+
+            window.location.hash = '#login';
+    }
+}
+
+export function initRouter() {
+    renderRoute();
+    window.addEventListener('hashchange', renderRoute);
+}
